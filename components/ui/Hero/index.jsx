cd27b3089f07f8832ac2fb3096dd4d9ecbdab3a7
yintaonk@qq.com <yintaonk@qq.com>
2025-06-01 17:04:01 +0800

    feat: highlight 'Easiest' and 'Hong Kong and China' in main title

--- components/ui/Hero/index.jsx
+++ components/ui/Hero/index.jsx
@@ -4,11 +4,11 @@ const Hero = () => (
     <section>
         <div className="custom-screen py-28 text-gray-600">
             <div className="space-y-5 max-w-4xl mx-auto text-center">
-                <h1 className="text-4xl text-gray-800 font-extrabold mx-auto sm:text-6xl">
-                    Build and scale your next business idea faster
+                <h1 className="text-4xl text-gray-800 font-extrabold mx-auto sm:text-6xl text-center">
+                    The <span style={{ color: "rgb(79,70,229)", fontWeight: "bold" }}>Easiest</span> Way to Register Your Company in <span style={{ color: "rgb(79,70,229)", fontWeight: "bold" }}>Hong Kong and China</span>
                 </h1>
                 <p className="max-w-xl mx-auto">
-                    Blinder making it simple for you to build and grow your SaaS applications, or any business idea.
+                    We simplify the complexities of establishing your business in Asia, offering expert guidance and end-to-end services so you can focus on what matters most: <span style={{ color: "rgb(31,44,55)", fontWeight: "bold" }}>Growth！</span>
                 </p>
                 <div className="flex items-center justify-center gap-x-3 font-medium text-sm">
                     <NavLink
