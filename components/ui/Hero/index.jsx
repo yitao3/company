bf6f9fa1817b27355bdbef179341bce001febe9d
yintaonk@qq.com <yintaonk@qq.com>
2025-06-01 18:47:18 +0800

    style: reduce vertical padding for hero section

--- components/ui/Hero/index.jsx
+++ components/ui/Hero/index.jsx
@@ -2,7 +2,7 @@ import NavLink from "../NavLink"
 
 const Hero = () => (
     <section>
-        <div className="custom-screen py-28 text-gray-600">
+        <div className="custom-screen py-12 text-gray-600">
             <div className="space-y-5 max-w-4xl mx-auto text-center">
                 <h1 className="text-4xl text-gray-800 font-extrabold mx-auto sm:text-6xl text-center">
                     The <span style={{ color: "rgb(79,70,229)", fontWeight: "bold" }}>Easiest</span> Way to Register Your Company in <span style={{ color: "rgb(79,70,229)", fontWeight: "bold" }}>Hong Kong and China</span>
