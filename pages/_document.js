9a53650cdf325617f7d4d69863438f8e5392a177
yitao3 <yintaonk@qq.com>
2025-06-02 15:58:14 +0800

    fix: update Google Analytics code format to match exactly

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className='scroll-smooth' lang='en'>
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4VNQ04006R"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4VNQ04006R');`
+          }}
+        />
+      </Head>
       <body className='md:overflow-visible'>
         <Main />
         <NextScript />
